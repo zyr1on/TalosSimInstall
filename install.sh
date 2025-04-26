d4d043d49bcca59653e90cfabc31a999dfeca0f0
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-04-26 13:45:19 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 wget https://filebin.net/dlydd67y3b3ydhqz/models-20250426T091428Z-001.zip
-unzip unzip models-20250426T091428Z-001.zip
+unzip models-20250426T091428Z-001.zip
 mv models/ src/cart_sim/
 sudo apt-get install ros-noetic-pcl-ros ros-noetic-gazebo-ros-pkgs ros-noetic-pcl-conversions gazebo9 libgazebo9-dev git curl
 catkin_make
