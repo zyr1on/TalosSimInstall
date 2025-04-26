5b9bbea06fccba126ee5ab9c874eb32acffad062
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-04-26 13:38:28 +0300

    Create installRosNoetic.sh

---
+++ installRosNoetic.sh
@@ -0,0 +1,9 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install ros-noetic-desktop
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install python3-rosdep
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
