63b1a47483738f4061b6e4e2461bf3f7e6ba1485
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-04-26 14:13:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ sudo apt-get install ros-noetic-pcl-ros ros-noetic-gazebo-ros-pkgs ros-noetic-pc
 wget https://filebin.net/dlydd67y3b3ydhqz/models-20250426T091428Z-001.zip
 unzip models-20250426T091428Z-001.zip
 mv models/ src/cart_sim/
+rm models-20250426T091428Z-001.zip
 catkin_make
 source devel/setup.bash
