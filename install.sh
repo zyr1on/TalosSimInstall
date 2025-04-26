36a99bfcdadd0c9962b06390bad64ee30587f9ab
Semih Özdemir <66450395+zyr1on@users.noreply.github.com>
2025-04-26 15:10:20 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 chmod +x *
-sudo apt-get install libqt5x11extras5-dev ros-noetic-pcl-ros ros-noetic-dynamic-reconfigure ros-noetic-roslint  ros-noetic-pcl-conversions ros-noetic-gazebo-ros-pkgs ros-noetic-pcl-conversions ros-noetic-roscpp ros-noetic-rospy ros-noetic-std-msgs ros-noetic-geometry-msgs  ros-noetic-joy  ros-noetic-message-generation 
+sudo apt-get install libqt5x11extras5-dev ros-noetic-fake-localization ros-noetic-pcl-ros ros-noetic-dynamic-reconfigure ros-noetic-roslint  ros-noetic-pcl-conversions ros-noetic-gazebo-ros-pkgs ros-noetic-pcl-conversions ros-noetic-roscpp ros-noetic-rospy ros-noetic-std-msgs ros-noetic-geometry-msgs  ros-noetic-joy  ros-noetic-message-generation 
 sudo apt-get install gazebo9 libgazebo9-dev git curl
 wget https://filebin.net/dlydd67y3b3ydhqz/models-20250426T091428Z-001.zip
 unzip models-20250426T091428Z-001.zip
